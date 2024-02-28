ff63d316c8cb6f0dfbe5f891b8c6c8cd63a1e602
Hector <hectorgool@github.com>
2024-02-28 12:06:55 -0600

    add DBDocument to schema

--- schema/schema.go
+++ schema/schema.go
@@ -4,4 +4,9 @@ type (
 	Document struct {
 		Id, Date, Transaction string
 	}
+	DBDocument struct {
+		Id          uint   `gorm:"primaryKey"`
+		Date        string `gorm:"type:date"`
+		Transaction string `json:"transaction"`
+	}
 )
